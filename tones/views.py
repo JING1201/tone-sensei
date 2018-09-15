eefaf7f97ba350fdc287fc50452b59aa7c5cc3b4
Gianne <gianneflores18@gmail.com>
2018-09-15 14:17:21 -0400

    testing out html with django

--- tones/views.py
+++ tones/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
-from django.http import HttpResponse
-
-
+# Create your views here
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'tones/index.html')
