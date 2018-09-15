2c11daf171000b6c140956287eaa4a49b8bae9d8
jing1201 <low.jing@hotmail.com>
2018-09-15 12:06:59 -0400

    Added transcription.py which includes function to transcribe from a remote audio file and function to extract tonal features of a text input

---
+++ transcription.py
@@ -0,0 +1,27 @@
+def transcribe_gcs(gcs_uri):
+    """Transcribes the audio file specified by the gcs_uri."""
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    client = speech.SpeechClient()
+
+    audio = types.RecognitionAudio(uri=gcs_uri)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=16000,
+        language_code='cmn-Hans-CN')
+
+    response = client.recognize(config, audio)
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+
+    return result.alternatives[0].transcript #returns the first alternative of the pronunciation
+
+def text_to_tone (transcript):
+    import pinyin
+    user_pronunciation = pinyin.get(transcript, format="numerical")
+    user_tones = [c for c in user_pronunciation if c.isDigit()]
+    return user_tones
